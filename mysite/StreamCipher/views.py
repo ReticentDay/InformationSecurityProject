787af421331c2e3253eab94d4066045b2079d191
silent <aa532199@gmail.com>
2017-12-30 15:18:28 +0800

    finish the test

--- mysite/StreamCipher/views.py
+++ mysite/StreamCipher/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def RsaEncrypt(e, N, m):
+    put = 1
+    for i in range(1,e):
+        put = ((put % N) * (m % N)) % N
+    return put
